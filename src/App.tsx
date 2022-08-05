9bbfc36e5f0b3dc7f48187dd39d9beb651e33971
Seungwoo Lee <timosean@naver.com>
2022-08-06 03:54:23 +0900

    feat: initial main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import "./index.css";
+
 import { Home } from "./pages/home";
 import { Main } from "./pages/main";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path=":username" element={<Main/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path=":username" element={<Main />} />
+    </Routes>
+  );
 }
 
 export default App;
