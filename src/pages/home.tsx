35771e6ce18693242fa6f907dd4ab7ec22085692
Seungwoo Lee <timosean@naver.com>
2022-08-06 02:07:35 +0900

    feat: add folder, handleinput function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Home: React.FC = () => {
-    
-    return (
-        <div>
-            <input type="text" className="nes-input" placeholder="username"/>
-            <button type="button" className="nes-btn is-primary">Start</button>
-        </div>
-    );
+  const [username, setUsername] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="nes-input"
+        placeholder="username"
+        onChange={handleInput}
+      />
+      <Link to={username}>
+        <button type="button" className="nes-btn is-warning">
+          Start
+        </button>
+      </Link>
+    </div>
+  );
 };
