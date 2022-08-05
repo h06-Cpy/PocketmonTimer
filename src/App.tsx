c1f071b6d005e795d000d1031ab715cf11a97f87
Soonho Kwon <percy3368@gmail.com>
2022-08-06 01:12:37 +0900

    feat: Project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/home";
+import { Main } from "./pages/main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <img src="/pokeball.png" className="logo" alt="Vite logo" />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path=":username" element={<Main/>}/>
+        </Routes>
+    );
 }
 
 export default App;
