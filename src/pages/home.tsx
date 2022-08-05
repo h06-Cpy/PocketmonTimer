fa24baad2c894b5d7a9f74b272e8f08849170c32
Soonho Kwon <percy3368@gmail.com>
2022-08-06 01:43:03 +0900

    feat: Add nes.css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,11 @@ import React from "react";
 export const Home: React.FC = () => {
     
     return (
-        <div>this is home</div>
+        <div>
+            <div className="nes-field">
+                <label htmlFor="name_field">Your name</label>
+                <input type="text" id="name_field" className="nes-input"/>
+            </div>
+        </div>
     );
-}
+};
